64ef9ac4847a093c8799ec5719ae6f2f381b7fce
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-06-22 10:13:03 +0530

    [Bhagyaraju] UC2 branch

--- lineComparisonProblem.java
+++ lineComparisonProblem.java
@@ -1,15 +1,23 @@
+import java.util.*;
 public class lineComparisonProblem{
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
-		int x1,x2,y1,y2;
+		int x11,x12,y11,y12,x01,x02,y01,y02;
 		Scanner in = new Scanner(System.in);
-		x1 = in.nextInt();
-		y1 = in.nextInt();
-		x2 = in.nextInt();
-		y2 = in.nextInt();
-		x = x2-x1; 
-		y = y2-y1;
+		x11 = in.nextInt();x01 = in.nextInt();
+		y11 = in.nextInt();y01 = in.nextInt();
+		x12 = in.nextInt();x02 = in.nextInt();
+		y12 = in.nextInt();y02 = in.nextInt();
+		double Line1=LengthOfTheLine(x11,x12,y11,y12);
+		double Line2=LengthOfTheLine(x01,x02,y01,y02);
+		if(Line1==Line2){
+			System.out.println("Both lines are equal");
+		}
+	}
+	public static  double LengthOfTheLine(int x1,int x2,int y1,int y2){
+		int x = x2-x1;
+		int y = y2-y1;
 		double Length_of_a_Line = Math.sqrt((x*x)+(y*y));
-		System.out.println(Length_of_a_Line);
+		return Length_of_a_Line;
 	}
 }
